d5bb4a170fb48c218ec3e2f87b2dd85ba5289f84
GuaiYiHu <guaiyihu@foxmail.com>
2018-11-15 15:37:16 +0000

    Set product model dynamically

--- init/init_whyred.cpp
+++ init/init_whyred.cpp
@@ -28,11 +28,14 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <fstream>
 #include <cstdio>
 
 #include <android-base/file.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "property_service.h"
 
@@ -43,6 +46,22 @@ using android::init::property_set;
 
 namespace android {
 namespace init {
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+     pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+ void property_override_dual(char const system_prop[],
+        char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
 
 static void init_alarm_boot_properties()
 {
@@ -77,9 +96,27 @@ static void init_alarm_boot_properties()
     }
 }
 
+
+static void init_setup_model_properties()
+{
+    std::ifstream fin;
+    std::string buf;
+     fin.open("/proc/cmdline");
+    while (std::getline(fin, buf, ' '))
+        if (buf.find("androidboot.hwc") != std::string::npos)
+            break;
+    fin.close();
+     if (buf.find("CN") != std::string::npos) {
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Redmi Note 5");
+    } else {
+        property_override_dual("ro.product.model", "ro.vendor.product.model",  "Redmi Note 5 Pro");
+    }
+}
+
 void vendor_load_properties()
 {
     init_alarm_boot_properties();
+    init_setup_model_properties();
 }
 }  // namespace init
 }  // namespace android
